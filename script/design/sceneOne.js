4b378af9c5f37c6ef7ea171afd5fdc25ce5a352f
Bintang Kurniawan <bintang.kurniawannn.98@gmail.com>
2025-11-10 18:28:43 +0700

    scene 2

--- script/design/sceneOne.js
+++ script/design/sceneOne.js
@@ -12,7 +12,18 @@ export function sceneOne(tl) {
 }
 
 function gsapEntry(tl) {
-  tl = gsap.timeline();
+  const timeObj = { h: 0, m: 0 };
+
+  //? FIRST APPEARANCE
+  tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".div-s1", // element to watch
+      start: "center center",
+      end: "+=4000",
+      scrub: true, // makes it smooth + tied to scroll
+      markers: true, // show start/end markers (for debugging)
+    },
+  });
   tl.fromTo(
     ".s1-hand",
     {
@@ -21,34 +32,21 @@ function gsapEntry(tl) {
     },
     {
       y: 27,
-      duration: 1,
+      duration: 2,
     }
-  );
-
-  const timeObj = { h: 0, m: 0 };
-
-  //? FIRST APPEARANCE
-  tl = gsap.timeline({
-    scrollTrigger: {
-      trigger: ".div-s1", // element to watch
-      start: "center center",
-      end: "+=2000",
-      scrub: true, // makes it smooth + tied to scroll
-      markers: true, // show start/end markers (for debugging)
-    },
-  });
-  tl.to(".s1-notif:nth-child(-n+3)", {
-    opacity: 1,
-    scale: 1,
-    duration: 1.5,
-    stagger: 0.4,
-    ease: "power3.out",
-  })
+  )
+    .to(".s1-notif:nth-child(-n+3)", {
+      opacity: 1,
+      scale: 1,
+      duration: 2.5,
+      stagger: 0.5,
+      ease: "power3.out",
+    })
     .to(
       ".s1-hand",
       {
         rotate: 15,
-        duration: 2,
+        duration: 3,
         ease: "power2.out",
       },
       "<"
@@ -56,8 +54,8 @@ function gsapEntry(tl) {
     .to(".s1-notif:nth-child(n+4):nth-child(-n+6)", {
       opacity: 1,
       scale: 1,
-      duration: 1.5,
-      stagger: 0.4,
+      duration: 2.5,
+      stagger: 0.5,
       ease: "power3.out",
     })
     .to(
@@ -65,7 +63,7 @@ function gsapEntry(tl) {
       {
         rotate: -25,
         y: 76,
-        duration: 2,
+        duration: 3,
         ease: "power2.out",
       },
       "<"
@@ -73,21 +71,21 @@ function gsapEntry(tl) {
     .to(".s1-hand", {
       rotate: 0,
       y: 27,
-      duration: 2,
+      duration: 3,
       ease: "power2.out",
     })
     .to(
       ".s1-hand",
       {
         y: "200vh",
-        duration: 3,
+        duration: 5,
         ease: "power2.out",
       },
       ">2"
     )
     .to(".s1-notif:nth-child(1)", {
       x: 100,
-      duration: 1.5,
+      duration: 3,
       ease: "power2.out",
     })
     .to(
@@ -95,7 +93,7 @@ function gsapEntry(tl) {
       {
         x: 140,
         y: -100,
-        duration: 1.5,
+        duration: 3,
         ease: "power2.out",
       },
       "<"
@@ -105,7 +103,7 @@ function gsapEntry(tl) {
       {
         x: 50,
         y: 70,
-        duration: 1.5,
+        duration: 3,
         ease: "power2.out",
       },
       "<"
@@ -116,7 +114,7 @@ function gsapEntry(tl) {
         x: -500,
         y: 180,
         scale: 0.7,
-        duration: 1.5,
+        duration: 3,
         ease: "power2.out",
       },
       "<"
@@ -126,7 +124,7 @@ function gsapEntry(tl) {
       {
         x: -140,
         y: -100,
-        duration: 1.5,
+        duration: 3,
         ease: "power2.out",
       },
       "<"
@@ -136,7 +134,7 @@ function gsapEntry(tl) {
       {
         x: -50,
         y: 70,
-        duration: 1.5,
+        duration: 3,
         ease: "power2.out",
       },
       "<"
@@ -149,7 +147,7 @@ function gsapEntry(tl) {
       },
       {
         y: 200,
-        duration: 2,
+        duration: 3,
         ease: "power2.out",
       },
       "<"
@@ -162,20 +160,20 @@ function gsapEntry(tl) {
       },
       {
         y: 200,
-        duration: 2,
+        duration: 3,
         ease: "power2.out",
       },
       "<"
     )
     .to(".time", {
       y: 100,
-      duration: 2,
+      duration: 3,
       ease: "power2.out",
     })
     .to(timeObj, {
       h: 15,
       m: 59,
-      duration: 2,
+      duration: 3,
       ease: "none",
       onUpdate: () => {
         const h = String(Math.floor(timeObj.h)).padStart(2, "0");
@@ -187,7 +185,7 @@ function gsapEntry(tl) {
       ".time",
       {
         color: "#ADAFB3",
-        duration: 2,
+        duration: 3,
         ease: "power2.inOut",
       },
       "<"
@@ -196,7 +194,7 @@ function gsapEntry(tl) {
       ".section-one",
       {
         backgroundColor: "#20262D",
-        duration: 2,
+        duration: 3,
         ease: "power2.inOut",
         immediateRender: false,
       },
@@ -205,16 +203,17 @@ function gsapEntry(tl) {
     .to(".s1-notif:nth-child(-n+6)", {
       opacity: 0,
       scale: 0.5,
-      duration: 2,
+      duration: 3,
       stagger: 0.4,
       ease: "power3.out",
-    })
+    });
+  tl.to({}, { duration: 3 })
     .to(
       ".s1-hp",
       {
         y: "200vh",
         opacity: 1,
-        duration: 2,
+        duration: 3,
         ease: "power2.out",
       },
       "<"
@@ -224,7 +223,7 @@ function gsapEntry(tl) {
       {
         h: 24,
         m: 59,
-        duration: 2,
+        duration: 3,
         ease: "none",
         onUpdate: () => {
           const h = String(Math.floor(timeObj.h)).padStart(2, "0");
@@ -239,7 +238,7 @@ function gsapEntry(tl) {
       {
         scale: 3,
         y: () => window.innerHeight / 2 - document.querySelector(".time").offsetHeight / 2,
-        duration: 2,
+        duration: 3,
         ease: "power3.out",
       },
       "<"
@@ -248,7 +247,7 @@ function gsapEntry(tl) {
       ".section-one",
       {
         backgroundColor: "#000",
-        duration: 2,
+        duration: 3,
         ease: "power2.inOut",
         immediateRender: false,
       },
@@ -256,7 +255,7 @@ function gsapEntry(tl) {
     )
     .to(".time", {
       opacity: 0,
-      duration: 2,
+      duration: 3,
       ease: "power2.inOut",
     })
     .fromTo(
@@ -267,14 +266,16 @@ function gsapEntry(tl) {
       },
       {
         y: 0,
-        duration: 2,
+        duration: 3,
         ease: "power4.out",
       },
       "<"
     )
+    .to({}, { duration: 6 })
+
     .to([".bubText1", ".bubText2"], {
       y: "-200vh",
-      duration: 2,
+      duration: 3,
       ease: "power4.out",
     })
     .fromTo(
@@ -285,16 +286,18 @@ function gsapEntry(tl) {
       },
       {
         y: 0,
-        duration: 2,
+        duration: 3,
         ease: "power4.out",
       },
       "<"
     )
     .to([".bubbleText3", ".bubbleText4"], {
       y: "-200vh",
-      duration: 2,
+      duration: 3,
       ease: "power4.out",
     })
+    .to({}, { duration: 3 })
+
     .fromTo(
       ".bubbleText5",
       {
@@ -303,20 +306,280 @@ function gsapEntry(tl) {
       },
       {
         y: 0,
-        duration: 2,
+        duration: 3,
         ease: "power4.out",
       },
       "<"
     )
-    .to(".bubbleText5 h1", { opacity: 0, duration: 2, ease: "power4.out" })
-    .to(".bubbleText5", { scale: 10, duration: 5, ease: "power4.inOut" })
+    .to({}, { duration: 6 })
+
+    .to(".bubbleText5 h1", { opacity: 0, duration: 3, ease: "power4.out" })
+    .to({}, { duration: 6 })
+
+    .to(".bubbleText5", { scale: 10, duration: 6, ease: "power4.inOut" })
     .to(".section-one", {
       backgroundColor: "#D3D9E0",
       duration: 0.5,
       ease: "power2.inOut",
       immediateRender: false,
     })
-    .to(".bubbleText5", { scale: 0, y: "-200vh", duration: 1, ease: "power4.inOut" }, "<");
+    .to(".bubbleText5", { scale: 0, y: "-200vh", duration: 2, ease: "power4.inOut" }, "<");
+
+  const textElement = document.querySelector(".typing-text");
+  const text = "sulit sekali untuk berkonsentrasi...";
+  textElement.innerHTML = text
+    .split("")
+    .map((char) => `<span class="char">${char}</span>`)
+    .join("");
+  gsap.set(".char", { opacity: 0 });
+
+  tl.fromTo(
+    ".s2-1",
+    {
+      x: "-100vw",
+    },
+    { x: 0, duration: 3, ease: "power4.out" }
+  )
+    .to(
+      ".char",
+      {
+        opacity: 1,
+        duration: 3,
+        stagger: 0.05,
+        ease: "none",
+      },
+      "<0.5"
+    )
+    .to({}, { duration: 6 })
+    .to(".s2-1", { x: "-100vw", duration: 6, ease: "power4.out", delay: 5 })
+    .to(
+      ".char",
+      {
+        opacity: 0,
+        duration: 3,
+        stagger: 0.05,
+        ease: "none",
+      },
+      "<0.5"
+    )
+    .to(".section-one", {
+      backgroundColor: "#4C4E52",
+      duration: 3,
+      ease: "power2.inOut",
+      immediateRender: false,
+    });
+
+  const textElement2 = document.querySelector(".typing-text2");
+  const text2 = "Dulu, aku bisa fokus pada momen-momen indah. Apa yang berubah..?";
+  textElement2.innerHTML = text2
+    .split("")
+    .map((char) => `<span class="char2">${char}</span>`)
+    .join("");
+  gsap.set(".char2", { opacity: 0 });
+
+  tl.to({}, { duration: 6 })
+
+    .fromTo(
+      ".s2-2",
+      {
+        x: "-100vw",
+      },
+      { x: 0, duration: 3, ease: "power4.out" }
+    )
+    .to(
+      ".char2",
+      {
+        opacity: 1,
+        duration: 3,
+        stagger: 0.05,
+        ease: "none",
+      },
+      "<0.5"
+    )
+    .to({}, { duration: 6 })
+
+    .to(".s2-2", { x: "-100vw", duration: 6, ease: "power4.out", delay: 5 })
+    .to(
+      ".char2",
+      {
+        opacity: 0,
+        duration: 3,
+        stagger: 0.05,
+        ease: "none",
+      },
+      "<0.5"
+    )
+    .to(".section-one", {
+      backgroundColor: "#D3D9E0",
+      duration: 3,
+      ease: "power2.inOut",
+      immediateRender: false,
+    });
+
+  const textElement3 = document.querySelector(".typing-text3");
+  const text3 = "Langkah pertama ciptakan ruang yang tenang..";
+  textElement3.innerHTML = text3
+    .split("")
+    .map((char) => `<span class="char3">${char}</span>`)
+    .join("");
+  gsap.set(".char3", { opacity: 0 });
+
+  tl.to({}, { duration: 6 })
+
+    .fromTo(
+      ".s2-3",
+      {
+        x: "-100vw",
+      },
+      { x: 0, duration: 3, ease: "power4.out" }
+    )
+    .to(
+      ".char3",
+      {
+        opacity: 1,
+        duration: 3,
+        stagger: 0.05,
+        ease: "none",
+      },
+      "<0.5"
+    )
+    .to({}, { duration: 6 })
+
+    .to(".s2-3", { x: "-100vw", duration: 6, ease: "power4.out", delay: 5 })
+    .to(
+      ".char3",
+      {
+        opacity: 0,
+        duration: 3,
+        stagger: 0.05,
+        ease: "none",
+      },
+      "<0.5"
+    );
+
+  const textElement4 = document.querySelector(".typing-text4");
+  const text4 = "Tenangkan pikiran. Tarik napas, hembuskan.";
+  textElement4.innerHTML = text4
+    .split("")
+    .map((char) => `<span class="char4">${char}</span>`)
+    .join("");
+  gsap.set(".char4", { opacity: 0 });
+
+  tl.to({}, { duration: 6 })
+
+    .fromTo(
+      ".s2-4",
+      {
+        x: "-100vw",
+      },
+      { x: 0, duration: 3, ease: "power4.out" }
+    )
+    .to(
+      ".char4",
+      {
+        opacity: 1,
+        duration: 3,
+        stagger: 0.05,
+        ease: "none",
+      },
+      "<0.5"
+    )
+    .to({}, { duration: 6 })
+
+    .to(".s2-4", { x: "-100vw", duration: 6, ease: "power4.out", delay: 5 })
+    .to(
+      ".char4",
+      {
+        opacity: 0,
+        duration: 3,
+        stagger: 0.05,
+        ease: "none",
+      },
+      "<0.5"
+    );
+
+  const textElement5 = document.querySelector(".typing-text5");
+  const text5 = "Sedikit demi sedikit, fokusku meningkat.";
+  textElement5.innerHTML = text5
+    .split("")
+    .map((char) => `<span class="char5">${char}</span>`)
+    .join("");
+  gsap.set(".char5", { opacity: 0 });
+
+  tl.to({}, { duration: 6 })
+
+    .fromTo(
+      ".s2-5",
+      {
+        x: "-100vw",
+      },
+      { x: 0, duration: 3, ease: "power4.out" }
+    )
+    .to(
+      ".char5",
+      {
+        opacity: 1,
+        duration: 3,
+        stagger: 0.05,
+        ease: "none",
+      },
+      "<0.5"
+    )
+    .to({}, { duration: 6 })
+
+    .to(".s2-5", { x: "-100vw", duration: 6, ease: "power4.out", delay: 5 })
+    .to(
+      ".char5",
+      {
+        opacity: 0,
+        duration: 3,
+        stagger: 0.05,
+        ease: "none",
+      },
+      "<0.5"
+    );
+
+  const textElement6 = document.querySelector(".typing-text6");
+  const text6 = "Dan kini, aku bisa lebih hadir untuk hal-hal yang benar-benar penting.";
+  textElement6.innerHTML = text6
+    .split("")
+    .map((char) => `<span class="char6">${char}</span>`)
+    .join("");
+  gsap.set(".char6", { opacity: 0 });
+
+  tl.to({}, { duration: 6 })
+
+    .fromTo(
+      ".s2-6",
+      {
+        x: "-100vw",
+      },
+      { x: 0, duration: 3, ease: "power4.out" }
+    )
+    .to(
+      ".char6",
+      {
+        opacity: 1,
+        duration: 3,
+        stagger: 0.05,
+        ease: "none",
+      },
+      "<0.5"
+    )
+    .to({}, { duration: 6 })
+
+    .to(".s2-6", { x: "-100vw", duration: 6, ease: "power4.out", delay: 5 })
+    .to(
+      ".char6",
+      {
+        opacity: 0,
+        duration: 3,
+        stagger: 0.05,
+        ease: "none",
+      },
+      "<0.5"
+    )
+    .to({}, { duration: 6 });
 }
 
 function gsapExit(tl) {}
